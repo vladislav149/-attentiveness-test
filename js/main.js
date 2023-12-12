b85affc91669793a6264dd433bcdcd99c8c49fe9
vladislav149 <vladislav149@mail.ru>
2023-12-12 17:25:19 +0300

    finish MVP

--- js/main.js
+++ js/main.js
@@ -1,11 +1,44 @@
 const UI = {
-  SVG_ELEMENT: document.querySelectorAll('.svg-elem')
+  SVG_ELEMENT: document.querySelectorAll('.svg-elem'),
+  SPAN_RESULT: document.querySelector('.js-result'),
+  DIV_RESULT: document.querySelector('.js-div-result')
 }
+const FINISHING_NUMBER = 90
+let currentCount = 0
 
+incrementCount()
 UI.SVG_ELEMENT.forEach(e => e.addEventListener('click', selectBlock))
 
-let currentCount = 1
-
 function selectBlock() {
-  console.log(+this.textContent.trim())
+  const elemWithText = this.nextElementSibling
+  const onlyTextFromElem = elemWithText.textContent.trim()
+  const numberFromText = Number(onlyTextFromElem)
+  checkCount(numberFromText)
+}
+
+function checkCount(number) {
+  if (isFinish(number)) return endGame()
+  const isValidNumber = number === currentCount
+  if (isValidNumber) return incrementCount()
+  showErrorSelect()
+}
+
+function incrementCount() {
+  const DELAY = 1000
+  currentCount++
+  UI.SPAN_RESULT.textContent = currentCount
+  UI.DIV_RESULT.classList.add('bg-result')
+  setTimeout(() => UI.DIV_RESULT.classList.remove('bg-result'), DELAY)
+}
+
+function isFinish(number) {
+  return number === FINISHING_NUMBER
+}
+
+function showErrorSelect(number) {
+  alert(`не верно, вы должны найти ${currentCount}`)
+}
+
+function endGame() {
+  alert('Вы выиграли, перезапустите игру')
 }
